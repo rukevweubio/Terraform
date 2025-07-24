4a5793a302951a915c70fdc00479513858cf22a5
ubiorukevwe@hgmail.com <ubiorukevwe@gmail.com>
2025-07-24 09:49:04 -0400

    Moved Terraform files into vpc-peering folder

--- user_data_mysql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y mysql-server
-sudo systemctl enable mysql
-sudo systemctl start mysql
--- user_data_nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install -y nginx
-sudo systemctl enable nginx
-sudo systemctl start nginx
